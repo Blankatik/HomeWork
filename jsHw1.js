507cb7f58a15f0e0e8606d9f490565ee220d0c9b
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-01-13 00:16:45 +0200

    Update jsHw1.js

--- jsHw1.js
+++ jsHw1.js
@@ -1,7 +1,11 @@
-function multByFactor(value, multiplier = 3) {
-     return value * multiplier
+//####Задача 1
+//
+//Перепишите код, заменив оператор `if` на тернарный оператор `?`
+
+
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
 }
-
-console.log(multByFactor(10, 2))
-
-console.log(multByFactor(5))
