3e2f4e6c90f5780de564205da0576561fdc905e8
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-01-18 18:53:21 +0200

    master

--- jsHw5.js
+++ jsHw5.js
@@ -1,16 +1,17 @@
 /* Перепишите код, заменив цикл `for` на `while`, без изменения поведения цикла.
-
-```js
+js
 for (var i = 0; i < 3; i++) {
 console.log(i);
 }
-``` */
+ */
+
 //ВІДПОВІДЬ:
 var i = 0;
 while (i < 3) {
-     console.loge(i);
+     console.log(i);
      i++;
 }
+
 /* 
 var i = 0;
 while (i < 3) {
