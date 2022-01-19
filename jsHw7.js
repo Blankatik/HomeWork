a3c547ca56be2ea5d2f852757de1e87ed59532dc
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-01-19 22:18:50 +0200

    Update jsHw7.js

--- jsHw7.js
+++ jsHw7.js
@@ -8,3 +8,19 @@ var arr = [1,2,3,4];
 **Внимание**!
 
 - Не разрашается использовать специальные методы массивов. */
+/* for (var arr = 1; arr < 5; arr++) {
+
+     if (!(arr % 2)) {
+       alert( arr );
+     }
+   
+   } */
+   var arr = [1,2,3,4];
+function arraySum(array){
+var sum = 0;
+for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+console.log(sum);
+}
+arraySum(arr);
