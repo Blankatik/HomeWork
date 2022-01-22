1703ebea9ff462fe00f60197321ead9b53f72eaf
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-01-23 00:32:13 +0200

    Update jsHw1.js

--- jsHw1.js
+++ jsHw1.js
@@ -1,11 +1,15 @@
 //####Задача 1
 //Перепишите код, заменив оператор `if` на тернарный оператор `?`
-let a = 1, b = 1
-//var result;
-//if (a + b < 4) {
-//result = true;
-//} else {
-//result = false;
-//}
+/* let a = 1, b = 1
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+} */
 // ВІДПОВІДЬ:
+let a = 1, b = 1
+
 let result = (a + b) < 4 ? true : false;
+
+console.log(result);
