72b67d0a2584910d0b297a5d39fcbddbd065799e
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-01-23 23:16:16 +0200

    Update jsHw3_3.js

--- Lesson_3/jsHw3_3.js
+++ Lesson_3/jsHw3_3.js
@@ -8,12 +8,12 @@ f(9,3,2); // 3
 f('s',9,3) // Error: all parameters type should be a Number */
 
 
-/* 
-let a = 0;
-let b = 0;
-let c = 0;
 
-function f() {
+/* let a = 0 // необовязково у даному випадку;
+let b = 0;
+let c = 0; */
+
+function f(a, b, c) {
      let result;
      
           if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number') {
@@ -25,8 +25,9 @@ function f() {
      }
 
 f(9, 3, 2);
-f('s',9,3); */
+f('s',9,3);
 
+/* 
 var sum = function(a, b, c) {
      if (typeof a === 'number' && typeof b === 'number' && typeof c === 'number') {
           return (a - b) / c;
@@ -34,6 +35,9 @@ var sum = function(a, b, c) {
           throw new Error('all parameters type should be a Number');
      }
 }
-let result = sum(9, 3, 2);
+var result = sum(9, 3, 2);
 console.log(result);
-let result = sum('s',9,3);
+var result = sum('s',9,3);
+console.log(result);
+
+ */
