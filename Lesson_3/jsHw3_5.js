3b52e0c459ec0313317444ac7df95053f5dc37c3
Blankatik <aleksandr.karnaukhov@ukr.net>
2022-02-05 22:54:55 +0200

    Update jsHw3_5.js

--- Lesson_3/jsHw3_5.js
+++ Lesson_3/jsHw3_5.js
@@ -15,4 +15,29 @@ isPositive('s') // Error: parameter type is not a Number
 var arr = [8];
 arr.push(2);
 console.log(arr[1]); // выведет число 2 */
-
+var randomMassive = [1, 2, -4, 3, -9, -1, 7];
+function isPositive() {
+     let result;
+     if (typeof arguments[i] === 'number') {
+          if (number < 0) {
+               return 'false'
+             } else {
+               return 'true'
+             }
+     } else {
+          throw new Error('messege type is not a number');
+     }
+}
+function fd(number) {
+     let result
+        if (number < 0) {
+               result = 'false';
+               console.log(result);
+           } else {
+               result = 'true';
+               console.log(result);
+           }
+}
+fd(5)
+function newMassive() {
+     for (let i = 0; i < arr.length; i++) {
